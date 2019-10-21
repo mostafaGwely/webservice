48b8ba7c78fff6a59f2e4d2e14dc26f6962692aa
mostafa <mostafa@DESKTOP-7KF1QC4>
2019-10-21 15:00:49 +0200

    Add project files.

---
+++ webservice2/student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace webservice2
+{
+    public class student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int  Age{ get; set; }
+        public int totalMarks { get; set; }
+    }
+}
